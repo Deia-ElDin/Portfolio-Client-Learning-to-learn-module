84e1de1e46e80ccf8fbee8ec6afa1aa0b1bdd3ba
Deia Hamad <deia.tech2021@gmail.com>
2024-10-21 12:18:01 +0400

    updated RightHome

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-  )``;
+  );
 }
 
 export default App;
--- src/features/home/RightHome.js
+++ src/features/home/RightHome.js
@@ -4,10 +4,12 @@ const RightHome = () => {
       <h1>
         I'm <span>Deia</span>.
       </h1>
-      <h3>A Front-End Web Developer.</h3>
+      <h3>A Full-Stack Web Developer.</h3>
       <p className="text-p">
-        Hi, I'm a web developer based in Abu Dhabi, UAE. <br />I have a passion
-        for web design and love to create for web and mobile devices.
+        Hi, I'm a web developer based in Abu Dhabi, UAE. <br />
+        I have a passion for web design and love to create for web and mobile
+        devices. <br />
+        I'm also passionate about AI and enjoy building AI-powered websites.
       </p>
     </article>
   );
